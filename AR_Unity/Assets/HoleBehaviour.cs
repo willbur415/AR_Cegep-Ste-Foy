72cc13e13c64b1dd9df79ceb218fca80b69292d1
Vincent Montminy <vingt_cent_20@hotmail.com>
2016-02-09 16:09:36 -0500

    prefabs

---
+++ AR_Unity/Assets/HoleBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleBehaviour : MonoBehaviour {
+
+    void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.gameObject.tag == "Player")
+        {
+            _collider.transform.position = GameObject.Find("Starting position").transform.position;
+        }
+    }
+}
